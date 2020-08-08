79894654c18e8bbd89d4a759dd27bdf060456446
Rodrigo Almeida <rodrigollnallna@gmail.com>
2020-08-08 12:41:39 -0300

    Master raiz

---
+++ lib/data/dummy_frases.dart
@@ -0,0 +1,17 @@
+
+import 'package:biscoito_sorte/models/frase.dart';
+
+const DUMMY_FRASES = {
+  '0': const Frase(
+      id_frase: '0',
+      frase: 'Frase 0'
+  ),
+  '1': const Frase(
+    id_frase: '1',
+    frase: 'Frase 1'
+  ),
+  '2': const Frase(
+      id_frase: '2',
+      frase: 'Frase 2'
+  ),
+};
---
+++ lib/models/frase.dart
@@ -0,0 +1,9 @@
+class Frase{
+  final String id_frase;
+  final String frase;
+
+ const Frase({
+    this.id_frase,
+    this.frase
+  });
+}
